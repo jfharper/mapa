34e957cdbf875b50819dd3383f00aff2f682291d
Jakub Florian <jfharper@seznam.cz>
2025-07-07 00:08:33 +0200

    fix3

--- constants.js
+++ constants.js
@@ -1,17 +1,5 @@
 // JavaScript Document
-const seznamTras = [
-        {
-          url: "/20090801-04%20-%20Sumava%20%28Antygl%20-%20Zaton%29.xml",
-          barva: this.petBarev,
-        },
-        { url: "/20120616-18%20-%20Sazava%20%28Sobesin%20-%20Pikovice%29.xml" },
-        {
-          url: "/20120812-14%20-%20Beskydy%20%28Frenstat%20-%20Bila%29.xml",
-          barva: this.triBarvy,
-        },
-        { url: "/20130523-26%20-%20Nova%20Pec%2C%20Plechy%2C%20Vltava.xml" },
-      ];
- const barvy = {
+const barvy = {
         modra: "#004c8c",
         oranzova: "#ff9b11",
         zluta: "#ffd625",
@@ -27,24 +15,37 @@ const seznamTras = [
       };
 			
  const barvyDefault = [
-        barvy.modra,
-        barvy.zluta,
-        barvy.cervena,
-        barvy.svetlemodra,
-        barvy.fialova,
-        barvy.oranzova,
-        barvy.fialova2,
-        barvy.cervena2,
-        barvy.svetlemodra2,
-        barvy.seda,
-        barvy.ruzova,
+        this.barvy.modra,
+        this.barvy.zluta,
+        this.barvy.cervena,
+        this.barvy.svetlemodra,
+        this.barvy.fialova,
+        this.barvy.oranzova,
+        this.barvy.fialova2,
+        this.barvy.cervena2,
+        this.barvy.svetlemodra2,
+        this.barvy.seda,
+        this.barvy.ruzova,
       ];
       const petBarev = [
-        barvy.fialova,
-        barvy.zelena,
-        barvy.seda,
-        barvy.cervena2,
-        barvy.ruzova,
+        this.barvy.fialova,
+        this.barvy.zelena,
+        this.barvy.seda,
+        this.barvy.cervena2,
+        this.barvy.ruzova,
       ];
-      const triBarvy = [barvy.oranzova, barvy.svetlemodra2, barvy.fialova2];     
-export {seznamTras, barvyDefault, petBarev, triBarvy}
+      const triBarvy = [this.barvy.oranzova, this.barvy.svetlemodra2, this.barvy.fialova2];
+const seznamTras = [
+        {
+          url: "/20090801-04%20-%20Sumava%20%28Antygl%20-%20Zaton%29.xml",
+          barva: this.petBarev,
+        },
+        { url: "/20120616-18%20-%20Sazava%20%28Sobesin%20-%20Pikovice%29.xml" },
+        {
+          url: "/20120812-14%20-%20Beskydy%20%28Frenstat%20-%20Bila%29.xml",
+          barva: this.triBarvy,
+        },
+        { url: "/20130523-26%20-%20Nova%20Pec%2C%20Plechy%2C%20Vltava.xml" },
+      ];
+      
+export {seznamTras, barvyDefault}

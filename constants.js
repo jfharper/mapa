90356eb9965fe52593199b252baec6632c3479ac
Jakub Florian <jfharper@seznam.cz>
2026-01-16 23:55:22 +0100

    Switch to new version

--- constants.js
+++ constants.js
@@ -348,7 +348,7 @@ const routeList = [
   {url: "/20251018%20-%20Noutonicky%20podzimnik%2013.gpx", barva: [barvy.ruzova]},
   {url: "/20251019%20-%20Ryzovna%20-%20Ryzovna.gpx", barva: [barvy.zluta]},
   {url: "/20251027-28%20-%20Horni%20Slavkov%20-%20Becov%20n.%20Teplou.gpx", barva: petBarev},
-  {url: "/20251115-16%20-%20Hyskov%20-%20Nizbor.gpx"},
+  {url: "/20251115-16%20-%20Hyskov%20-%20Nizbor.gpx", barva: [barvy.seda, barvy.fialova]},
 ];
 
 export { routeList, colorsDefault, waypointOptions };
